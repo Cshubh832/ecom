a9bbcad9b0da1cac85dbcaea201f02880ac968ce
Pradeep Kumar <dev12@bytescrum.com>
2021-03-19 18:31:00 +0530

    second push

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('frontend.index');
 // });
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/','App\Http\Controllers\FrontendController@index');
 
 Route::get('/checkout','App\Http\Controllers\FrontendController@checkout');
@@ -25,21 +28,27 @@ Route::get('/product_list','App\Http\Controllers\FrontendController@product_list
 Route::get('/wishlist','App\Http\Controllers\FrontendController@wishlist');
 Route::get('/contact','App\Http\Controllers\FrontendController@contact');
 Route::get('/cart','App\Http\Controllers\FrontendController@cart');
-Route::get('/login','App\Http\Controllers\FrontendController@login');
+// Route::get('/login','App\Http\Controllers\FrontendController@login');
 
 // Admin ////////////////////////////////////////////////////////////////////////
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin','verified']], function () {
-    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
-    // Route::get('profile', 'DashboardController@showProfile')->name('profile');
-    // Route::put('profile', 'DashboardController@updateProfile')->name('profile.update');
-    // Route::put('profile/password', 'DashboardController@changePassword')->name('profile.password');
-    // Route::resource('user', 'UserController')->except(['create', 'show', 'edit', 'store']);
-    // Route::resource('category', 'CategoryController')->except(['create', 'show', 'edit']);
-    // Route::resource('post', 'PostController');
-    // Route::get('/comments', 'CommentController@index')->name('comment.index');
-    // Route::delete('/comment/{id}', 'CommentController@destroy')->name('comment.destroy');
-    // Route::get('/reply-comments', 'CommentReplyController@index')->name('reply-comment.index');
-    // Route::delete('/reply-comment/{id}', 'CommentReplyController@destroy')->name('reply-comment.destroy');
-    // Route::get('/post-liked-users/{post}', 'PostController@likedUsers')->name('post.like.users');
-});
+// Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin','verified']], function () {
+//     // Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+//     // Route::get('profile', 'DashboardController@showProfile')->name('profile');
+//     // Route::put('profile', 'DashboardController@updateProfile')->name('profile.update');
+//     // Route::put('profile/password', 'DashboardController@changePassword')->name('profile.password');
+//     // Route::resource('user', 'UserController')->except(['create', 'show', 'edit', 'store']);
+//     // Route::resource('category', 'CategoryController')->except(['create', 'show', 'edit']);
+//     // Route::resource('post', 'PostController');
+//     // Route::get('/comments', 'CommentController@index')->name('comment.index');
+//     // Route::delete('/comment/{id}', 'CommentController@destroy')->name('comment.destroy');
+//     // Route::get('/reply-comments', 'CommentReplyController@index')->name('reply-comment.index');
+//     // Route::delete('/reply-comment/{id}', 'CommentReplyController@destroy')->name('reply-comment.destroy');
+//     // Route::get('/post-liked-users/{post}', 'PostController@likedUsers')->name('post.like.users');
+// });
 
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
